188691ce0f06fa8ae9a62d242bd56a3229f7e589
tcannetti <tcannetti@gmail.com>
2020-12-01 15:48:29 -0600

    number functions done

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,18 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+if(num % 2 === 0){
+  return true;
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+ return arr.reduce((a, b) => a + b, 0)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  // arr.forEach(number => console.log(number))
 }
 
 module.exports = {
